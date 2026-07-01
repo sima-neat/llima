e29e1f4578e8a1b3014d7f74ffffa8404fa4e74b
florian.voss <florian.voss@sima.ai>
2026-07-01 08:42:40 +0000

    simplify Whisper runtime for new artifacts

--- sima_lmm/devkit/cpp/whisper_config.hpp
+++ sima_lmm/devkit/cpp/whisper_config.hpp
@@ -24,7 +24,7 @@ struct WhisperConfig{
     // Fields added by sima-lmm during whisper_config.json file generation.
     std::string model_name;
     bool decoder_use_future_token_mask;
-    bool language_detect_enabled = false;
+    bool language_detect_enabled;
     std::vector<uint32_t> language_token_ids;
     std::vector<std::string> language_codes;
 
@@ -46,9 +46,9 @@ inline void from_json(const nlohmann::json& j, WhisperConfig& v) {
     j.at("vocab_size").get_to(v.vocab_size);
     j.at("decoder_use_future_token_mask").get_to(v.decoder_use_future_token_mask);
 
-    v.language_detect_enabled = j.value("language_detect_enabled", false);
-    v.language_token_ids = j.value("language_token_ids", std::vector<uint32_t>{});
-    v.language_codes = j.value("language_codes", std::vector<std::string>{});
+    j.at("language_detect_enabled").get_to(v.language_detect_enabled);
+    j.at("language_token_ids").get_to(v.language_token_ids);
+    j.at("language_codes").get_to(v.language_codes);
 }
 
 
